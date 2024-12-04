dd07fcd06a443eeb585485441f190cd6c90ebc02
Cristian Jetomo <120572492+ji-chani@users.noreply.github.com>
2024-12-04 15:39:47 +0800

    Update phca.py
    can handle different homology dimensions

--- modules/phca.py
+++ modules/phca.py
@@ -9,7 +9,8 @@ import warnings
 warnings.filterwarnings('ignore')
 
 class PHCA:
-    def __init__(self, dim=0):
+    """ Persistent Homology Classification Algorithm """
+    def __init__(self, dim:list=[0]):
         self.dim = dim
     
     def fit(self, X_train, y_train):
@@ -18,10 +19,10 @@ class PHCA:
     
     def predict(self, X_test):
         y_pred = []
-        for i,x_new in enumerate(tqdm(X_test)):
+        for x_new in tqdm(X_test):
             self.byClass_data_plus = update_data(self.byClass_data, x_new)
             self.PD_byClass_plus = get_persistence_diagram(self.byClass_data_plus, self.dim)
-            self.selected_class = class_category_selector(self.PD_byClass, self.PD_byClass_plus, self.dim)
+            self.selected_class = class_selector(self.PD_byClass, self.PD_byClass_plus, self.dim)
             y_pred.append(self.selected_class)
         return y_pred
 
@@ -33,8 +34,8 @@ def by_class_data(X, y):
         byClass_data[y[i]].append(X[i])
     return byClass_data
 
-def get_persistence_diagram(data, dim):
-    return {key: ripser.ripser(np.array(data[key]), maxdim=dim)['dgms'] for key in data.keys()}
+def get_persistence_diagram(data, dim:list):
+    return {key: ripser.ripser(np.array(data[key]), maxdim=max(dim))['dgms'] for key in data.keys()}
 
 def update_data(data:dict, x_new:list):
     data_plus = copy.deepcopy(data)
@@ -42,15 +43,15 @@ def update_data(data:dict, x_new:list):
         data_plus[clss].append(x_new)
     return data_plus
 
-def get_lifespan(PD:list, dim:int):
+def get_lifespan(PD:list, dim:list):
     total_lifespan = 0
-    for d in range(0, dim+1):
+    for d in dim:
         births, deaths = PD[d][:,0], PD[d][:,1]
         births, deaths = births[np.isfinite(deaths)], deaths[np.isfinite(deaths)]  # remove inf values
         total_lifespan += np.sum(deaths) - np.sum(births)
     return total_lifespan
 
-def class_category_selector(orig_PD:dict, new_PD:dict, dim:int):
+def class_selector(orig_PD:dict, new_PD:dict, dim:list):
     scores = {}
     for clss in orig_PD.keys():
         orig_lspan, new_lspan = get_lifespan(orig_PD[clss], dim), get_lifespan(new_PD[clss], dim)
