e67e5a3084f9101cf5355544059307ae684c821d
Cristian Jetomo <120572492+ji-chani@users.noreply.github.com>
2026-06-06 14:22:46 +0800

    updated hand_landmarker module

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,5 +1,6 @@
 from modules.fsl_alphabet_dataset import FilipinoSignLanguage
-from modules.mediapipe_landmarks import Image2Landmarks, normalize_landmarks
+# from modules.mediapipe_landmarks import Image2Landmarks, normalize_landmarks
+from modules.mediapipe_landmarks_new import Image2Landmarks, normalize_landmarks
 from modules.classification_modules import kfoldDivideData, prepared_data, plot_boxplots, plot_bars, plot_confusion_matrix, classification_report_with_specificity, nemenyi_test
 from modules.phca import PHCA
 from modules.printing import TeeOutput
